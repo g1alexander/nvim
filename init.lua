70c04d21638f63ba6ab935a9d3cc8c7868b57718
g1alexander <me@g1alexander.com>
2024-11-07 22:16:47 -0500

    lsp config

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
+require("config.vim-options")
 require("config.lazy")
