8a6dd15bf96519ae6c5b1e90625c3079feea4aef
g1alexander <me@g1alexander.com>
2024-11-12 21:52:54 -0500

    add copilot plugin and new session management and terminal toggle plugins

---
+++ lua/plugins/sessions.lua
@@ -0,0 +1,19 @@
+return {
+  'rmagatti/auto-session',
+  config = function ()
+    require("auto-session").setup({
+      session_lens = {
+        buftypes_to_ignore = {},
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = true,
+        mappings = {
+          delete_session = { "i", "<leader>dd" },
+        }
+      },
+      vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
+        noremap = true,
+      }),
+    })
+  end
+}
